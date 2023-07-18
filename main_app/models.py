2ab5dee481ef1cf49d91257825a44f100e1be123
larrytu327 <larrytu327@gmail.com>
2023-07-17 22:27:39 -0700

    added bootswatch theme

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class MessageBoard(models.Model):
+    subject = models.CharField(max_length=300)
+    date_added = models.TimeField(auto_now_add=True)    
+    posts = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.subject
+    
+    class Meta:
+        ordering = ['date_added', 'subject']
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    ##on_delete CASCADE, if the profile is deleted, than everything under it gets deleted
+    first_name = models.CharField(max_length=255, default='')
+    last_name = models.CharField(max_length=255, default='')
+    phone = models.CharField(max_length=255, default='')
+    city = models.CharField(max_length=255, default='')
+    type  = models.CharField(max_length=255, default='')
+    parents = models.CharField(max_length=255, default='')
+    students = models.CharField(max_length=255, default='')
+    teachers = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.first_name
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
 from django.views import View # <- View class to handle requests
-from django.http import HttpResponse # <- a class to handle sending a type of response
+from django.http import HttpResponse 
+from .models import MessageBoard
 
 
 # Create your views here.
@@ -10,5 +11,13 @@ class Home(TemplateView):
     template_name = "home.html"
 
 
-class About(View):
+class About(TemplateView):
     template_name = "about.html"
+
+class MessageBoards(TemplateView):
+    template_name = "messageboards.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["messageboards"] = MessageBoard.objects.all()
+        return context
